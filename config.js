b961cf4145a34a534de637538873c2b7b8658f30
jhham96 <logbug1004@gmail.com>
2026-09-11 14:19:25 +0700

    프론트엔드 서버 동기화 체크로직 추가

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ window.ENV = {
     process.env &&
     process.env.NEXT_PUBLIC_GAS_URL
       ? process.env.NEXT_PUBLIC_GAS_URL
-      : "https://script.google.com/macros/s/AKfycbz491LhwQs-W6i7Zwn5Oodv2mpGD5pOXV6NDZs0Sgye1kocdUx0nWC0fuqDgfwMyBtRkA/exec",
+      : "https://script.google.com/macros/s/AKfycbwbhEfEluL19j3hvSKGtxoGAUCNeYpvroANzuV5P0AB7X_E5SEt4T2NXanyt1Iakbzn/exec",
 };
